2ec9e8476564385f04250b7c08a8a905cf83363b
rster2002 <rster2002dev@gmail.com>
2019-09-04 11:43:24 +0200

    Initial Commit

---
+++ week1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age:");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name} and you are {age}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ week1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give your age:");
+            string ageInput = Console.ReadLine();
+
+            int age = int.Parse(ageInput);
+
+            age = age + 1;
+
+            string output = age.ToString();
+            Console.WriteLine($"Next year your age is {age}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
