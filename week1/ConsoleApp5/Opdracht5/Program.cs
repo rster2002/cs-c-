861024f8b627b4faa11779ab3e36016517bb9d4d
rster2002 <rster2002dev@gmail.com>
2019-09-04 12:55:25 +0200

    Done 1.5

--- week1/ConsoleApp5/Opdracht5/Program.cs
+++ week1/ConsoleApp5/Opdracht5/Program.cs
@@ -10,6 +10,32 @@ namespace Opdracht5
     {
         static void Main(string[] args)
         {
+
+            Random rnd = new Random();
+
+            int[] numbers = { rnd.Next(101, 10000), rnd.Next(101, 10000), rnd.Next(101, 10000), rnd.Next(101, 10000) };
+
+            Console.WriteLine("Add the following numbers together:");
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+
+            int answer;
+            bool isCorrectType = int.TryParse(Console.ReadLine(), out answer);
+
+            if (isCorrectType)
+            {
+                Console.WriteLine($"That's the { (answer == numbers.Sum() ? "right" : "wrong") } answer");
+            } else
+            {
+                Console.WriteLine("The input was a string, not a number");
+            }
+            
+
+            Console.ReadKey();
+
         }
     }
 }
