c5d2a04d4b9b95289cbad8a9f5c730aed2b1db49
Bjørn Reemer <bjornsivar@gmail.com>
2019-09-20 06:34:41 +0200

    Done 3.6, 3.7

---
+++ week3/Week3/Opdracht6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht6 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Geef je scoren: ");
+            int score = int.Parse(Console.ReadLine());
+
+            char grade = 'F';
+
+            if (score >= 90 && score <= 100) {
+                grade = 'A';
+            } else if (score >= 80 && score <= 89) {
+                grade = 'B';
+            } else if(score >= 70 && score <= 79) {
+                grade = 'C';
+            } else if(score >= 60 && score <= 69) {
+                grade = 'D';
+            } else if(score < 60) {
+                grade = 'F';
+            }
+
+            Console.WriteLine("Je heb een {0}, daarmee heb je dit vak{1}behaald.", grade, score < 70 ? " niet " : " ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week3/Week3/Opdracht7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht7 {
+    class Program {
+        static void Main(string[] args) {
+            double healthyBMIMin, healthyBMIMax;
+
+            double BMI(double _weight, double _length) {
+                return _weight / (Math.Pow(_length, 2));
+            }
+
+            Console.Write("Wat is je gewicht in kg: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.Write("Geef je length in cm: ");
+            double length = double.Parse(Console.ReadLine()) / 100;
+
+            Console.Write("Wat is je geslacht (m/v): ");
+            string sex = Console.ReadLine().ToUpper();
+
+            if (sex == "M") {
+                healthyBMIMin = 20;
+                healthyBMIMax = 25;
+            } else if (sex == "V") {
+                healthyBMIMin = 19;
+                healthyBMIMax = 24;
+            } else {
+                Console.WriteLine("Dat is geen geldig geslacht.");
+                Console.ReadKey();
+
+                return;
+            }
+
+            double userBMI = BMI(weight, length);
+
+            if (userBMI >= healthyBMIMin && userBMI <= healthyBMIMax) {
+                Console.Write("Je heb een gezond gewicht");
+            } else if (userBMI > healthyBMIMax) {
+                Console.Write("Je bent te zwaar");
+            } else {
+                Console.Write("Je bent te licht");
+            }
+
+            Console.WriteLine(", je heb namelijk een BMI van {0}", Math.Round(userBMI, 2));
+            Console.WriteLine($"Een gezond BMI voor jouw geslacht ligt tussen de {healthyBMIMin} en {healthyBMIMax}");
+            Console.ReadKey();
+        }
+    }
+}
