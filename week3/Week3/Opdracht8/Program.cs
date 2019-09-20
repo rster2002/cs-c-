8b068546d219b39fb1b99d8ac484a674b9abf9e3
Bjørn Reemer <bjornsivar@gmail.com>
2019-09-20 06:43:22 +0200

    Done 3.8 Sorted usings for 3.3, 3.4, 3.6, 3.7

---
+++ week3/Week3/Opdracht8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Opdracht8 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Hoeveel uur is de metaaldraaibank al in gebruik (hele uren): ");
+            int hoursUsed = int.Parse(Console.ReadLine());
+
+            Console.Write("Hoe oud is de metaaldraaibank (in hele jaren): ");
+            int yearsInUse = int.Parse(Console.ReadLine());
+
+            Console.Write("Hoeveel storingen zijn er geweest dit jaar: ");
+            int defectsThisYear = int.Parse(Console.ReadLine());
+
+            if (hoursUsed > 10000 || yearsInUse > 7 || defectsThisYear > 25) {
+                Console.WriteLine("De metaaldraaibank moet worden vervangen");
+            } else {
+                Console.WriteLine("De metaaldraaibank hoeft nog niet te worden vervangen");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
