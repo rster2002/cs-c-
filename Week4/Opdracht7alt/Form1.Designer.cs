30c69c71de3edfa40764428cbcc44ad98cb97ae7
rster2002 <rster2002dev@gmail.com>
2019-10-03 21:16:21 +0200

    Added alt version of Opdracht7

---
+++ Week4/Opdracht7alt/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Opdracht7 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textboxSideSize = new System.Windows.Forms.TextBox();
+            this.labelBox = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zijde";
+            // 
+            // textboxSideSize
+            // 
+            this.textboxSideSize.Location = new System.Drawing.Point(117, 6);
+            this.textboxSideSize.Name = "textboxSideSize";
+            this.textboxSideSize.Size = new System.Drawing.Size(100, 20);
+            this.textboxSideSize.TabIndex = 1;
+            // 
+            // labelBox
+            // 
+            this.labelBox.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBox.Location = new System.Drawing.Point(15, 65);
+            this.labelBox.Name = "labelBox";
+            this.labelBox.Size = new System.Drawing.Size(202, 178);
+            this.labelBox.TabIndex = 2;
+            this.labelBox.Text = "x";
+            this.labelBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Maak vierkant";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelBox);
+            this.Controls.Add(this.textboxSideSize);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textboxSideSize;
+        private System.Windows.Forms.Label labelBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
