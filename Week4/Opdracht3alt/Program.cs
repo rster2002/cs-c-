05edd1d0ee2f40d7b6a3b1ce3bc6a371c1b3db22
Bjørn Reemer <bjornsivar@gmail.com>
2019-10-03 15:12:50 +0200

    Added 4.3 alt 1

--- Week4/Opdracht3alt/Program.cs
+++ Week4/Opdracht3alt/Program.cs
@@ -1,33 +1,30 @@
 ﻿using System;
 
-namespace Opdracht3 {
+namespace Opdracht3alt {
     class Program {
-
-        static int getAndValidate() {
-            if (int.TryParse(Console.ReadLine(), out int number)) {
-                return number;
-            } else {
-                Console.WriteLine("Not a number");
-                return getAndValidate();
-            }
-        }
-
         static void Main(string[] args) {
+            // Initializeer variabelen
             int inputNumber;
             int numberTotal = 0;
             int iteration = 1;
 
+            // do-while loop omdat we zo minimaal 1x de code uitvoeren
             Console.WriteLine("Geef getallen: ");
             do {
-                inputNumber = getAndValidate();
+                // Lees input van de gebruiker
+                inputNumber = int.Parse(Console.ReadLine());
 
+                // Check of het getal niet 0 is en of de iteratie een veelvoud van 5 is
                 if (inputNumber != 0 && iteration % 5 == 0) {
+                    // Voeg het ingelezen getal toe bij je totaal
                     numberTotal += inputNumber;
                 }
 
+                // Increment de iteratie variable
                 iteration++;
             } while (inputNumber != 0);
 
+            // Laat de som van ed getallen zien
             Console.WriteLine($"De som van de getallen is {numberTotal}");
             Console.ReadKey();
         }
